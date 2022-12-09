c0b491520d422285f564ca41c5683058ab61d2a0
Jason Chafin <jchafin@ucsc.edu>
2022-12-09 09:57:06 -0800

    Add two scripts

---
+++ scripts/single-site/open-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Export site name as environment variable SITE_NAME:
+# `export SITE_NAME="my-site-name"`
+# This script uses a local `secrets.sh` file
+source secrets.sh
+SITE="$(terminus site:list --name=$SITE_NAME --format=list --field=Name)"
+# Open Test site
+echo "Firing up the $SITE TEST environment for review"
+terminus env:view $SITE.test
